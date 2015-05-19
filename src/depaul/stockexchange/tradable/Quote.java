fd3d0f913a4971df8836d63c9eea73f4b86dc6e3
vmvc2v <old.far.hot@gmail.com>
2015-05-19 17:27:25 -0500

    finished Quote class

--- src/depaul/stockexchange/tradable/Quote.java
+++ src/depaul/stockexchange/tradable/Quote.java
@@ -1,5 +1,41 @@
 package depaul.stockexchange.tradable;
 
-public class Quote {
+import depaul.stockexchange.BookSide;
+import depaul.stockexchange.price.Price;
 
+public class Quote {
+	private String user;
+	private String product;
+	private QuoteSide buyQs;
+	private QuoteSide sellQs;
+
+	
+	public Quote(String userName, String productSymbol, Price buyPrice, 
+			int buyVolume, Price sellPrice, int sellVolume) throws InvalidTradableValue {
+		this.user = userName;
+		this.product = productSymbol;
+		this.buyQs = new QuoteSide(userName, productSymbol, buyPrice, buyVolume, BookSide.BUY);
+		this.sellQs = new QuoteSide(userName, productSymbol, sellPrice, sellVolume, BookSide.SELL);
+	}
+	
+	public String getUserName() {
+		return this.user;
+	}
+	
+	public String getProduct() {
+		return this.product;
+	}
+	
+	public QuoteSide getQuoteSide(BookSide sideIn) throws InvalidTradableValue {
+		if (sideIn == BookSide.BUY) {
+			return new QuoteSide(this.buyQs);
+		} else {
+			return new QuoteSide(this.sellQs);
+		}
+	}
+	
+	public String toString() {
+		return String.format("%s quote: %s - %s ", 
+				this.getUserName(), this.buyQs, this.sellQs);
+	}
 }
