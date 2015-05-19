9375861a1b8c6ef34214e8062bfe3291280f2dde
vmvc2v <old.far.hot@gmail.com>
2015-05-19 14:14:55 -0500

    finished Order class.

--- src/depaul/stockexchange/tradable/Order.java
+++ src/depaul/stockexchange/tradable/Order.java
@@ -1,5 +1,31 @@
 package depaul.stockexchange.tradable;
 
-public class Order {
+import depaul.stockexchange.BookSide;
+import depaul.stockexchange.price.*;
 
+public class Order extends TradableImplement implements Tradable {
+	
+	public Order(String userName, String productSymbol, 
+			Price orderPrice, int originalVolume, BookSide side) 
+					throws InvalidTradableValue {
+		this.setUser(userName);
+		this.setProduct(productSymbol);
+		this.setPrice(orderPrice);
+		this.setOriginalVolume(originalVolume);
+		this.setRemainVolume(originalVolume);
+		this.setSide(side);
+		this.buildId();
+	}
+
+	@Override
+	public boolean isQuote() {
+		return false;
+	}
+
+	@Override
+	protected void buildId() throws InvalidTradableValue {
+		this.id = String.format("%s%s%s%s", this.getUser(), this.getProduct(), 
+				this.getPrice(), System.nanoTime());
+	}
+	
 }
