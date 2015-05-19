742fd092a1b3696740ef4ccd965711f65d36fe10
vmvc2v <old.far.hot@gmail.com>
2015-05-19 12:32:58 -0500

    update Tradable interface.

--- src/depaul/stockexchange/tradable/Tradable.java
+++ src/depaul/stockexchange/tradable/Tradable.java
@@ -1,5 +1,17 @@
 package depaul.stockexchange.tradable;
 
-public interface Tradable {
+import depaul.stockexchange.price.*;
 
+public interface Tradable {
+	String getProduct();
+	Price getPrice();
+	int getOriginalVolume();
+	int getRemainVolume();
+	int getCancelledVolume();
+	void setCancelledVolume(int newCancelledVolume);
+	void setRemainVolume(int mewRemainvolume);
+	String getUser();
+	String getSide();
+	boolean isQuote();
+	String getId();
 }
