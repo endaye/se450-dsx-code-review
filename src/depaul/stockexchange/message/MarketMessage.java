07ac5ec9be6469a1e0204159e44a3307ae49365f
Yuancheng Zhang <old.far.hot@gmail.com>
2015-05-20 21:33:20 -0500

    finished MarketMessage class

--- src/depaul/stockexchange/message/MarketMessage.java
+++ src/depaul/stockexchange/message/MarketMessage.java
@@ -2,8 +2,18 @@ package depaul.stockexchange.message;
 
 public class MarketMessage {
 
-	public MarketMessage() {
-		// TODO Auto-generated constructor stub
+	private MarketState state;
+	
+	public MarketMessage(MarketState state) {
+		this.setState(state);
+	}
+
+	public MarketState getState() {
+		return state;
+	}
+
+	private void setState(MarketState state) {
+		this.state = state;
 	}
 
 }
